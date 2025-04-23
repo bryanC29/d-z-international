854405032107d07f217da669edc2d93d784c94cf
Bryan Christy <bryanchristy2016@gmail.com>
2025-04-23 23:43:20 +0530

    FIX: CartItems

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['example.com', 'via.assets.so'],
+    domains: ['example.com', 'via.assets.so', 'media.croma.com'],
   },
 };
 
--- src/components/cartItem/page.tsx
+++ src/components/cartItem/page.tsx
@@ -3,8 +3,11 @@
 import { useQuery } from '@apollo/client';
 import Image from 'next/image';
 import { GET_PRODUCT_DETAILS } from '../../queries/getCartItems';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import client from '@/lib/apolloClient';
+import axios from 'axios';
+import { useAuth } from '@/app/context/authContext';
+import { useRouter } from 'next/navigation';
 
 interface CartItemProps {
   productId: string;
@@ -12,12 +15,15 @@ interface CartItemProps {
 }
 
 export default function CartItem({ productId, quantity }: CartItemProps) {
-  console.log(productId);
+  const { user } = useAuth();
+  const router = useRouter();
   const { data, loading, error } = useQuery(GET_PRODUCT_DETAILS, {
     client,
     variables: { pid: productId },
     skip: !productId,
   });
+
+  const [imgSrc, setImgSrc] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     if (error) {
@@ -25,33 +31,72 @@ export default function CartItem({ productId, quantity }: CartItemProps) {
     }
   }, [error]);
 
-  // Handle loading and error states
+  useEffect(() => {
+    if (data?.product?.media) {
+      setImgSrc(data.product.media[0]);
+    }
+  }, [data]);
+
   if (loading) return <p>Loading product details...</p>;
   if (error) return <p>Error loading product details.</p>;
 
-  // Extract product data from the response
-  const { name, media, price, description, details, size } =
-    data?.product || {};
+  const { name, price, description } = data.product;
+
+  const handleImgError = () =>
+    setImgSrc(
+      'https://via.assets.so/img.jpg?w=500&h=500&tc=white&bg=grey&t=Image'
+    );
+
+  const api = process.env.NEXT_PUBLIC_API || 'localhost';
+
+  const handleRemoveItem = async () => {
+    if (!user?.token) {
+      router.push('/login');
+    } else {
+      try {
+        const res = await axios.delete(`${api}/cart/${productId}`, {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+        if (res.status === 200) {
+          window.location.reload();
+        } else {
+          console.error('Error removing item from cart');
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
 
   return (
-    <div className="md:p-2 flex flex-row md:mx-10 text-white gap-2 md:gap-10 mb-2 md:justify-center p-1 bg-zinc-600 rounded-lg">
-      <div className="relative md:h-60 h-48 rounded-lg md:w-[20%] w-[45%] bg-cyan-700 overflow-hidden">
-        <Image
-          src={media || '/default-image.png'} // Fallback if no image is available
-          alt={name || 'Product Image'}
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-      <div className="flex flex-col md:flex-row md:gap-14 md:w-[70%]">
-        <div className="md:w-[100%]">
-          <p className="text-2xl m-1 mb-2 md:mt-4">{name}</p>
-          <p className="mx-1 md:mt-4">{description}</p>
-          <p className="text-lg m-1 md:mt-14">${price}</p>
-        </div>
-        <div className="my-5 mx-1 flex gap-14 justify-start md:w-[50%] md:flex-col">
-          <p>Size: {size}</p>
+    <div className="flex flex-col md:flex-row md:mx-10 mx-5 text-white md:mb-2 mb-5 bg-black border border-white hover:bg-gray-900 rounded-lg overflow-hidden">
+      <Image
+        src={imgSrc || ''}
+        alt={name || 'Product Image'}
+        width={500}
+        height={500}
+        onError={handleImgError}
+        className="md:w-1/4 w-full"
+      />
+      <div className="p-4 w-full">
+        <p className="text-2xl font-semibold">{name}</p>
+        <p className="p-2">{description}</p>
+        <div className="flex flex-col justify-around w-full md:flex-row my-4">
+          <p>Price: &#8377;{price}</p>
+          <p className="bg-orange-600 px-1 w-max">
+            Amount: &#8377;{price * quantity}
+          </p>
           <p>Quantity: {quantity}</p>
+        </div>
+        <div className="md:pt-6 py-2">
+          <button
+            onClick={handleRemoveItem}
+            className="border rounded-md border-red-600 py-2 px-4 hover:bg-red-600"
+          >
+            Remove item
+          </button>
         </div>
       </div>
     </div>
